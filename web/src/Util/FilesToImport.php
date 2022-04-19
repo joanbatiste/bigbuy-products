a765e316dbd3240ed874991e40bd3d297a12411f
joan.puig <joanpulle@gmail.com>
2022-04-19 09:39:58 +0200

    fix typo on filesImport

--- web/src/Util/FilesToImport.php
+++ web/src/Util/FilesToImport.php
@@ -6,6 +6,6 @@ namespace App\Util;
 trait FilesToImport
 {
     private $xlsxFile = "../public/files/Productos.xlsx";
-    private $xmlFile = "../public/files/Articulos.xlsx";
+    private $xmlFile = "../public/files/Articulos.xml";
     private $jsonFile = "../public/files/catalog.json";
 }
